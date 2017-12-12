cf02ed7a7ec7c086ddc7c0c772ba2138db8126d2
libaoquan95 <1282676967@qq.com>
2017-12-12 13:01:54 +0800

    构建用户-景点矩阵

--- createUserAttractionMatrix.py
+++ createUserAttractionMatrix.py
@@ -8,21 +8,9 @@ from pandas import Series, DataFrame
 # param  geoFile：geo数据集
 #        phontUserFile：写入照片-用户关联矩阵的文件
 # return none
-def createPhotoUserMatrix(geoFile, phontUserFile):
-    #读取数据集，获取经纬度信息
-    inFile = open(geoFile, encoding='utf-8')
-    datas = []
-    for line in inFile:
-        meteData = line.strip().split('\t')
-        datas.append([meteData[0], meteData[1], meteData[3]])
-    inFile.close()
-    
-    # 将照片-用户关联数据存入DataFrame类型photoUserFrame
-    photoUserData = np.array(datas)
-    data = {'userId': photoUserData[:, 1], 'takenDate': photoUserData[:, 2]}
-    photoUserFrame = DataFrame(data, columns=['userId', 'takenDate'],index = photoUserData[:, 0])
-    #print(photoUserFrame)
-    photoUserFrame.to_csv(phontUserFile, encoding='utf-8', index=True)
+def createPhotoUserMatrix(dataSets, phontUserFile):
+    dataSets[['USER_NAME','PHOTO_TIME']].to_csv(phontUserFile, \
+            encoding='utf-8', index=True)
     
     
 # 构建照片-景点关联矩阵，即合并所有省份的分类信息
@@ -48,14 +36,16 @@ def createPhotoAttractionMatrix(culstersFolder, phontAttractionFile):
     
     data = []
     for i in range(len(provinces)):
-        oneFrame = pd.read_csv(culstersFolder + provinces_py[i] + '.csv', index_col=0)
+        oneFrame = pd.read_csv(culstersFolder + provinces_py[i] + '.csv', \
+                               index_col=0)
         oneFrame['province'] = provinces_py[i]
         data.append(oneFrame)
     
     # 合并
     phontAttractionFrame = pd.concat(data)
     # 写入照片-用户景点矩阵文件
-    phontAttractionFrame.to_csv(phontAttractionFile, encoding='utf-8', index=True)
+    phontAttractionFrame[['clusterId','province']].to_csv(phontAttractionFile,\
+                        encoding='utf-8', index=True)
     
     
 # 构建用户-景点矩阵
@@ -68,37 +58,32 @@ def createUserAttractionMatrix(phontAttractionFile, phontUserFile, userAttractio
     #print(phontUserFrame)
     
     # 找出所有的用户
-    usersFrame = phontUserFrame.drop_duplicates('userId')
-    #print(usersFrame)
-    users = np.array(usersFrame['userId'])
-    #print(users)
+    usersFrame = phontUserFrame.drop_duplicates('USER_NAME')
+    users = np.array(usersFrame['USER_NAME'])
     
     # 构建用户-景点矩阵
     # 遍历用户信息
     userAttractionData = []
     for userId in users:
         # 找到与此用户关联的照片id
-        photoIds = np.array(phontUserFrame[phontUserFrame['userId'] == userId].index.drop_duplicates())
-        #print(userId)
-        #print(photoIds)
-        
+        photoIds = np.array(phontUserFrame[phontUserFrame['USER_NAME'] == userId].index.drop_duplicates())
         # 找到照片ID相关联的景点信息
         attractions = phontAttractionFrame.loc[photoIds, ['clusterId', 'province']]
-        attractions = attractions.dropna(subset=['province']) # 删除噪音数据
-        #attractionIds = attractions.drop_duplicates()
-        #print(attractionIds)
-        for (clusterId, province), group in attractions.groupby([attractions['clusterId'],\
-                                            attractions['province']]):
-            if(int(clusterId) != -1):
+        # 筛除噪音数据(clusterId == -1)
+        attractions = attractions[attractions['clusterId'] != -1]
+        
+        if(len(attractions) != 0):
+            # 获取景点id(省标识_聚类id)
+            #attractionIds = attractions.drop_duplicates()
+            for (clusterId, province), group in attractions.groupby([attractions['clusterId'],attractions['province']]):
                 attractionId = ('%s_%s' % (province, int(clusterId)))
                 userAttractionData.append([userId, attractionId, len(group)])
-    
     # 将用户-景点-照片数量关系写入DataFrame
     userAttractionData = np.array(userAttractionData)
-    data = {'userId': userAttractionData[:, 0], 'attractionId': userAttractionData[:, 1], \
+    data = {'USER_NAME': userAttractionData[:, 0],    \
+            'attractionId': userAttractionData[:, 1], \
             'photoCount': userAttractionData[:, 2]}
-    userAttraction = DataFrame(data, columns=['userId', 'attractionId', 'photoCount'])
-    #print(userAttraction)
+    userAttraction = DataFrame(data, columns=['USER_NAME', 'attractionId', 'photoCount'])
     # 写入照片-用户景点矩阵文件
     userAttraction.to_csv(userAttractionFile, encoding='utf-8', index=False)
     
@@ -106,16 +91,16 @@ def createUserAttractionMatrix(phontAttractionFile, phontUserFile, userAttractio
 def main():
     baseDir = 'yfcc100m_dataset/'
     
-    geoFile = baseDir + 'flick-geo-abortchina-china'
-    addressFile = baseDir + 'flick-geo-abortchina-china-address'
+    # 读取数据集
+    dataSets = pd.read_csv(baseDir + 'info_0_3.csv', index_col=0)
     
     phontUserFile = baseDir + 'photo-user.csv'
-    #createPhotoUserMatrix(geoFile, phontUserFile)
+    createPhotoUserMatrix(dataSets, phontUserFile)
     
     culstersFolder = baseDir + 'culsters/'
     phontAttractionFile = baseDir + 'photo-attraction.csv'
-    #createPhotoAttractionMatrix(culstersFolder, phontAttractionFile)
+    createPhotoAttractionMatrix(culstersFolder, phontAttractionFile)
     
     userAttractionFile = baseDir + 'user-attraction.csv' 
-    #createUserAttractionMatrix(phontAttractionFile, phontUserFile, userAttractionFile)
+    createUserAttractionMatrix(phontAttractionFile, phontUserFile, userAttractionFile)
 main()
